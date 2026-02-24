e0895545526fa102e5b00e5faa130d802c7aa855
unknown <hseoglan@naver.com>
2026-02-24 09:40:21 +0900

    5차 업데이트

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import "./Editor.css"
 import Button from './Button'
 import EmotionItem from './EmotionItem'
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom'
 import { getStringedDate } from '../util/getStringedDate'
 import { emotionList } from '../util/constants'
 
-const Editor = ({onSubmit}) => {
+const Editor = ({initData, onSubmit}) => {
     const emotionId=3
     const nav = useNavigate()
     
@@ -15,6 +15,15 @@ const Editor = ({onSubmit}) => {
         emotionId: 3,
         content: ""
     })
+
+    useEffect(() => {
+        if (initData) {
+            setInput({
+                ...initData,
+                createdDate: new Date(Number(initData.createdDate))
+            })
+        }
+    }, [initData])
 
     const onChangeInput = (e) => {
         let name = e.target.name
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,18 +1,59 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import Header from '../components/Header'
 import Button from '../components/Button'
 import Editor from '../components/Editor'
+import { DiaryDispatchContext, DiaryStateContext } from '../App'
 
 const Edit = () => {
-    const params = useParams()
+    const {id} = useParams()
+    const nav = useNavigate()
+    const {onDelete, onUpdate} = useContext(DiaryDispatchContext)
+    const data=useContext(DiaryStateContext)
+    const [curDiaryItem, setCurDiaryItem] = useState(null)
+
+    useEffect(() => {
+      const currentDiaryItem = data.find(
+        (item)=>String(item.id)===String(id)
+      )
+      setCurDiaryItem(currentDiaryItem)
+    }, [id, nav, data])
+
+
+
+    const onClickDelete = () => {
+      if (window.confirm('일기를 정말 삭제하시겠습니까?')) {
+        onDelete(id)
+        nav('/', {replace: true})
+      }
+    }
+
+    const onSubmit = (input) => {
+      if (window.confirm('일기를 수정하시겠습니까?')) {
+        onUpdate(
+          id,
+          input.createdDate,
+          input.emotionId,
+          input.content
+        )
+        nav('/', {replace: true})
+      }
+    }
 
   return (
     <div>
       <Header
-        leftChild={<Button text={'뒤로가기'}/>}
+        leftChild={<Button
+                      text={'뒤로가기'}
+                      onClick={()=> nav(-1)}/>}
         title={'일기 수정하기'}
-        rightChild={<Button text={'삭제하기'} type={'NEGATIVE'}/>}/>
+        rightChild={<Button
+                      text={'삭제하기'}
+                      onClick={onClickDelete}
+                      type={'NEGATIVE'}/>}/>
+      <Editor
+        initData={curDiaryItem}
+        onSubmit={onSubmit}/>
     </div>
   )
 }
